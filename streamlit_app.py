83931b826ae0dc5e44039cca7ea6523904bebeba
aamir12111 <108257164+aamir12111@users.noreply.github.com>
2023-07-27 16:00:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,5 +19,5 @@ def get_fruityvice_data(this_fruit_choice):
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
 
-import snowflake.connector
+
 
